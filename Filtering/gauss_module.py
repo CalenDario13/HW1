5ad48e53a97ef6ce208fe609bd872797889b37b5
CalenDario13 <dariorusso.dr13@gmail.com>
2020-10-19 13:05:19 +0200

    Complete the function gauss and add the unction resize_img to reshape the image matrix for the convolution

--- Filtering/gauss_module.py
+++ Filtering/gauss_module.py
@@ -12,8 +12,30 @@ The filter should be defined for all integer values x in the range [-3sigma,3sig
 The function should return the Gaussian values Gx computed at the indexes x
 """
 def gauss(sigma):
+    """
+    it generates the domain of the Function and its values.
     
-    #...
+    :type sigma: the standard deviation of the Normal distribution
+    :par sigma: float
+    
+    """  
+    
+    # Set parameters 
+
+    X  = np.arange(-3*int(sigma), 3*int(sigma) + 1)
+    Gx = np.zeros(X.shape)
+    m = X.shape[0] // 2
+    
+    # Build the array:
+    for x in range(-m, m+1):
+        t1 = 1/(sqrt(2 * pi) * sigma)
+        t2 = exp(-x**2  / 2 * sigma**2)
+        g = t1 * t2
+        Gx[x + m] = g
+    
+    # Normalization
+    w = np.sum(Gx)
+    Gx = 1/w * Gx
     
     return Gx, x
 
